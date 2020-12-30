2a62eb7268f242a9b3b81a1fcf061d14f8495281
samsiddy <usama.afzal@nyu.edu>
2020-12-30 13:01:13 +0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,33 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+makeVector <- function(x = numeric()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmean <- function(mean) m <<- mean
+  getmean <- function() m
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
+}
+
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  invmat <- NULL
+  set <- function(y){
+    x <<- y
+    invmat <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(solvemat) invmat <<- solvemat
+  getInverse <- function() invmat
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+}
 }
 
 
